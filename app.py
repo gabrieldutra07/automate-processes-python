160933e9c4619cc25a9f4cd31d912eae75291695
Gabriel Dutra <gabrielcbdutra@gmail.com>
2024-01-17 19:57:52 -0300

    testes com a automação.

---
+++ app.py
@@ -0,0 +1,9 @@
+import pyautogui
+from time import sleep
+
+pyautogui.click(379,869,duration=2)
+pyautogui.click(325,694,duration=2)
+pyautogui.write(cpf)
+pyautogui.click(314,743,duration=2)
+pyautogui.click(370,612,duration=2)
+
