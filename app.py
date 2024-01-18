2157bf227bf2d17263067ba41c95b935e8cdf0ae
Gabriel Dutra <gabrielcbdutra@gmail.com>
2024-01-18 12:15:21 -0300

    pendente realizar os testes para puxar o cpf do arquivo .txt

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
+
 import pyautogui
 from time import sleep
 
-pyautogui.click(379,869,duration=2)
-pyautogui.click(325,694,duration=2)
-pyautogui.write(cpf)
-pyautogui.click(314,743,duration=2)
-pyautogui.click(370,612,duration=2)
+pyautogui.click(404,862,duration=2)
+sleep(2)
+pyautogui.hotkey('command', 'n')
+sleep(1)
+pyautogui.write("https://solucoes.receita.fazenda.gov.br/Servicos/certidaointernet/PF/Emitir/")
+pyautogui.press('enter')
+sleep(1)
 
+with open('cpfs.txt', 'r') as file:
+    for line in file:
+        cpf = line
+        pyautogui.click(326,693, duration=2)
+        pyautogui.write(cpf)
+        pyautogui.click(326,693, duration=2)
+        pyautogui.click(314,740, duration=2)
+        pyautogui.click(327,682, duration=2)
